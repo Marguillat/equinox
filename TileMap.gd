b66c76fc119b3784ebbcba7e8388acaf0d386fc4
Margouillat <erwanduchene@gmail.com>
2024-01-23 13:00:37 +0100

    beaucoup de choses

--- TileMap.gd
+++ TileMap.gd
@@ -1,9 +1,22 @@
 extends TileMap
-@onready var tile_map = %TileMap
+@onready var tile_map = $"."
+@onready var heaven_paralax = %"heaven-paralax"
+@export var realm = "heaven"
 
 
 func _process(delta):
 	if Input.is_action_just_pressed("switch"):
-		tile_map.scale.y = (tile_map.scale.y) *-1
+		tile_map.scale.y = (tile_map.scale.y) * -1
+		heaven_paralax.scale.y = (heaven_paralax.scale.y)* -1
+		if heaven_paralax.offset.y == 1050 :
+			heaven_paralax.offset.y = 0
+		else:
+			heaven_paralax.offset.y = 1050
+			
+		
+		if realm == "heaven":
+			realm = "hell"
+		else:
+			realm = "heaven"
 
 #coucou
--- character.gd
+++ character.gd
@@ -3,17 +3,31 @@ extends CharacterBody2D
 #import de l'image rémanante
 @export var r_node : PackedScene
 @onready var timer_remanance = $TimerRemanance
+@onready var tile_map = $"../TileMap"
+@onready var animated_sprite_2d = %AnimatedSprite2D
 
-const SPEED = 300.0
+
+const SPEED = 500.0
 const dashSpeedMultiplicator = 1.5
 const JUMP_VELOCITY = -400.0
 var dooble_jump = 0
+var simple_dash = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 #process physics and movement
 func _physics_process(delta):
+	if is_on_floor():
+		simple_dash = 0
+	
+	#anim les personages
+	if (velocity.x > 1) || (velocity.x < -1) :
+		## on utilise play() au lieu de .animation pour éviter les beugs de première frame 
+		animated_sprite_2d.play("run-" + tile_map.realm)
+	else:
+		animated_sprite_2d.play("idle-"+ tile_map.realm)
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -22,8 +36,8 @@ func _physics_process(delta):
 	if (Input.is_action_just_pressed("jump") and is_on_floor()):
 		velocity.y = JUMP_VELOCITY
 		dooble_jump = 0
-	elif Input.is_action_just_pressed("jump") and dooble_jump == 0:
-		velocity.y = JUMP_VELOCITY
+	elif Input.is_action_just_pressed("jump") and dooble_jump == 0 and tile_map.realm == "heaven" :
+		velocity.y = JUMP_VELOCITY * 1.4
 		dooble_jump=dooble_jump+1
 
 	# Get the input direction and handle the movement/deceleration.
@@ -35,11 +49,13 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	move_and_slide()
+	var isLeft = velocity.x < 0
+	animated_sprite_2d.flip_h = isLeft
 
 #créée l'image rémanante
 func add_ghost():
 	var ghost = r_node.instantiate()
-	ghost.set_property(position,$Sprite2D.scale)
+	ghost.set_property(position,$AnimatedSprite2D.scale)
 	get_tree().current_scene.add_child(ghost)
 
 #quand timeout créée l'image rémanante
@@ -60,5 +76,6 @@ func dash():
 	gravity = previous_gravity
 
 func _input(event):
-	if event.is_action_pressed("dash"):
+	if event.is_action_pressed("dash") && (tile_map.realm == "hell") && simple_dash == 0:
 		dash()
+		simple_dash = 1
